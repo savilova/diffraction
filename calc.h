ebdf6d39bf4224d037400e8ed6177347298d60d5
savilova <n-adgerika@yandex.ru>
2017-04-07 21:17:45 +0300

    clean up

--- calc.h
+++
@@ -1,9 +0,0 @@
-#ifndef MY_LIBRARY_H_INCLUDED
-#define MY_LIBRARY_H_INCLUDED
-
-#include <cstdlib>
-
-//преобразование матрицы светового поля в плоскости Z_0 в матрицу в плоскости наблюдения
-void transform();
-
-#endif // MY_LIBRARY_H_INCLUDED
