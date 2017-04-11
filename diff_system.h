5ff2d516b1fc872bdc6c261190fa54843835f3f8
savilova <n-adgerika@yandex.ru>
2017-04-11 21:27:27 +0300

    started transform function

--- diff_system.h
+++ diff_system.h
@@ -10,10 +10,11 @@ public:
     int wave;
     float k;
     float z;
-    int M;
-    int N;
+    int row;
+    int col;
     int **f_0;
     int **f_z;
+    int Size;
 
     
     //конструктор
@@ -21,9 +22,10 @@ public:
         wave = 532;
         k  = 2*3.1416/wave;
         z = 2;
-        M = 50;
-        N = 50;
-        f_0 = f;            
+        row = 50;
+        col = 50;
+        f_0 = f;     
+        Size = 10;
         //TODO
 
 
@@ -37,8 +39,8 @@ public:
     //подсчет поля в плоскости z по принципу Гюйгенса-Френеля(интеграл)
     void transform(){
     std::cout << "Hello!This is transform function!" << std::endl; 
-    for (int i = 0; i<M; i++){
-        for(int j = 0; j<N; j++){
+    for (int i = 0; i<row; i++){
+        for(int j = 0; j<col; j++){
             std::cout << f_0[i][j];
         }
         std::cout << std::endl;
@@ -51,40 +53,12 @@ public:
         распечатывать пока только матрицу интенсивностей, как обычно в ostream.
         */
         //в длинном комменте - первая версия интеграла
-    /*
-        int lyambda, z;
-        const int Size = 10; 
-        int row,col;
-    
-        ifstream fin ("painting.txt");
-        string str;
-        getline(fin, str);
-        cout << str << endl;
 
-        int ** data = new int * [row];
-        for (int r=0; r < row; r++)
-        {
-        data [r] = new int [col];
-        for (int c=0; c < col; c++)
-        {
-
-            fin >> data[r][c];
-
-        }
-        }
-        fin.close();
-
-        for (int r=0 ;r<row ;r++)
-        {
-        for (int c = 0; c < col; c++)
-        {
-            cout << data[r][c];
-        }
-        }
         
-        const double PI =  3.141592;
-        int ** Re_G_table = new int * [row];
-        int ** Im_G_table = new int * [row];
+    
+        double PI =  3.141592;
+        int ** Re_G_table = new int * [50];
+        int ** Im_G_table = new int * [50];
         for (int x = 0; x < row; x++)
         {
         for (int y = 0; y < col; y++)
@@ -97,30 +71,39 @@ public:
                 {
                     int R = sqrt(pow(z,2)+pow((Size/1000)*(x-r),2)+pow((Size/1000)*(y-c),2));  
                     int cosinus = z/R;   
-                    int Re_per_Sum = data[r][c] * (cosinus/R) *cos(2*PI*pow(10,9)/(R*lyambda));
-                    int Im_per_Sum = data[r][c] * (cosinus/R) *sin(2*PI*pow(10,9)/(R*lyambda)); 
+                    int Re_per_Sum = f_0[r][c] * (cosinus/R) *cos(k*pow(10,9)/R);
+                    int Im_per_Sum = f_0[r][c] * (cosinus/R) *sin(k*pow(10,9)/R); 
                     Re_Sum += Re_per_Sum*pow((Size/1000),2);
                     Im_Sum += Im_per_Sum*pow((Size/1000),2);
                 }
             }
-            Re_G_table[x][y] = Re_Sum/lyambda;
-            Im_G_table[x][y] = Im_Sum/lyambda;
+            Re_G_table[x][y] = Re_Sum/wave;
+            Im_G_table[x][y] = Im_Sum/wave;
         }
         }
-        int ** intensive = new int * [row];
+        
         for (int x = 0; x < row; x++)  
         {
         for (int y = 0; y < col; y++)
         {
-            intensive[x][y] = pow(Re_G_table[x][y],2) + pow(Im_G_table[x][y],2);
+            f_z[x][y] = pow(Re_G_table[x][y],2) + pow(Im_G_table[x][y],2);
+            std::cout<<f_z[x][y];
         }
+            std::cout << std::endl;
         }
+
+        for (int i = 0; i < row; i++) 
+        {
+        delete [] Re_G_table[i];
+        }
+        delete [] Re_G_table;
+
+        for (int i = 0; i < row; i++) 
+        {
+        delete [] Im_G_table[i];
+        }
+        delete [] Im_G_table;
     
-        delete data;
-        delete Re_G_table;
-        delete Im_G_table;
-        delete intensive;
-    */
 }
 };
 
