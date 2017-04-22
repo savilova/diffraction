cfa22bd7d91662d1c73b35cc2d12443df46d7b35
savilova <n-adgerika@yandex.ru>
2017-04-22 15:24:56 +0300

    comments, format, minor fixes

--- diff_system.h
+++ diff_system.h
@@ -11,19 +11,15 @@ void draw_mat(int w, int h, int *data);
 class diff_system
 {
 public:
-    int scale, n_cols, n_rows;
-    int n_size;
+    int cell; //физическая длина dx(d_ksi) и dy(d_eta), мкм
+    int scale; //отношение стороны экрана к стороне препятствия
     int wave; //длина волны, нм
     double k; //волновое число, 1/нм
     double z; //расстояние от (0,0) на препятствии до (0,0) на экране, мкм
-    int rows; //число рассматриваемых отрезков dy
-    int cols; //число рассматриваемых отрезков dx
-    int m_size; //размер массива
-    int *f_0; //матрица прозрачности препятствия, единицы - ?
-    int *f_z; // матрица интенсивности в плоскости экрана, единицы - ?
-    int cell; //длина dx и dy, мкм
-
-
+    int o_cols, o_rows, o_size; // числа рассматриваемых отрезков d_ksi, d_eta и клеток в препятствии (obsticle)
+    int s_cols, s_rows, s_size; // числа рассматриваемых отрезков d_x, d_y и клеток на экране (screen)
+    int *f_0; //! скалярная амплитуда поля в плоскости z_0+, единицы - ?
+    int *f_z; //! интенсивность в плоскости экрана, единицы - ?
 
     //самый полный конструктор
     diff_system(int sc, int w, double z, int r, int c, int *f)
@@ -31,21 +27,20 @@ public:
         wave = w;
         k  = 2*3.1416/wave;
         this->z = z;
-        rows = r;
-        cols = c;
-        m_size = rows*cols;
+        o_rows = r;
+        o_cols = c;
+        o_size = o_rows*o_cols;
+        //! FIXME (Аня#2#): на самом деле f сейчас задает пропукаемость, а в f_0 нам нужно поле
         f_0 = f;
+        //! FIXME (Аня#1#): масштаб работает не так и только для квадратов
         scale = sc;
-        n_size = scale*scale*m_size;
-        n_cols = scale*cols;
-        n_rows = scale*rows;
-        f_z = new int [n_size];
-        for(int i=0; i<n_size; i++) f_z[i]=255;
-        cell = 10;
-        draw_mat(cols,rows,f_0);
-        draw_mat(n_cols,n_rows,f_z);
+        s_size = pow(scale,2)*o_size;
+        s_cols = scale*o_cols;
+        s_rows = scale*o_rows;
+        f_z = new int [s_size];
+        for(int i=0; i<s_size; i++) f_z[i]=0; //по умолчанию экран не освещен
+        cell = 100; //микроны
 
-        //TODO
     }
 
     //деструктор
@@ -60,17 +55,13 @@ public:
     {
         cout << "Hello!This is transform function!" << endl;
 
-        double * Re_G_table = new double [n_size];
-        double * Im_G_table = new double [n_size];
+        double * Re_G_table = new double [s_size];
+        double * Im_G_table = new double [s_size];
 
-        cout << "tables initialized" << endl;
-
-        double Re_Sum = 0;
-        double Im_Sum = 0;
+        double Re_Sum, Im_Sum;
         double Re_per_Sum = 0;
         double Im_per_Sum = 0;
         double R = z;
-
 
         int ksi = 0;
         int eta = 0;
@@ -80,51 +71,62 @@ public:
         cout << "variables initialized" << endl;
         cout << "starting transform..." << endl;
 
-        for (int i = 0; i < n_size; i++) //перебор точек в плоскости экрана (x,y)
+        for (int i = 0; i < s_size; i++) //перебор точек в плоскости экрана (x,y)
         {
+            //! FIXME (Аня#1#): отцентрировать экран - сейчас препятствие задвинуто в левый верхний угол проекции экрана
 
-            x = cell*(i%n_cols);
-            y = cell*((i - x)/n_cols);
+            // ищем физические координаты центра площадки dx*dy
+            x =  cell/2 + cell*(i%s_cols);
+            y =  cell/2 + cell*((i - i%s_cols)/s_cols);
 
+            //обнуляем поле в данной точке экрана
+            Re_Sum = 0;
+            Im_Sum = 0;
 
-
-
-
-            for (int j = 0; j < m_size; j++) //перебор точек в плоскости f_0 (ksi,eta)
+            //интегрирование для одной точки экрана
+            for (int j = 0; j < o_size; j++) //перебор точек в плоскости f_0 (ksi,eta)
             {
-                ksi = cell*(j%cols);
-                eta = cell*((j-ksi)/cols);
 
+                // ищем физические координаты центра площадки d_ksi*d_eta
+                ksi = cell*(j%o_cols);
+                eta = cell*((j-j%o_cols)/o_cols);
+
+                //считаем расстояние от точки на препятствии до точки на экране
                 R = sqrt(pow(z,2)+pow(x-ksi,2)+pow(y-eta,2));
 
+                //! (1/i*wave) внесен под знак интеграла
+                Re_per_Sum = f_0[j] * z * sin(1000*k*R)/(wave*R*R); //вклад одной точки препятствия в поле текущей точки на экране
+                Re_Sum += Re_per_Sum; //суммирование по всем точкам препятствия
 
-                Re_per_Sum = f_0[j] * z * sin(1000*k*R)/(wave*R*R);
-                Re_Sum += Re_per_Sum;
-
-                Im_per_Sum = f_0[j] * z * cos(1000*k*R)/(wave*R*R);
+                Im_per_Sum = f_0[j] * z * cos(1000*k*R)/(wave*R*R); // то же для мнимой части амплитуды
                 Im_Sum += Im_per_Sum;
 
             }
 
+            //значение суммарного поля в точке на экране
             Re_G_table[i] = Re_Sum;
             Im_G_table[i] = Im_Sum;
-            Re_Sum = 0;
-            Im_Sum = 0;
 
-        if ((int)(100*i/n_size)!=((int)(100*(i-1)/n_size))) cout << (int)(100*i/n_size) << "% calculations completed"<< endl;
+            // прогресс бар
+            // it`s not stupid if it works :)
+            if ((int)(100*i/s_size)!=((int)(100*(i-1)/s_size))) cout << (int)(100*i/s_size) << "% calculations completed"<< endl;
         }
         cout<<"End of field calculations"<< endl;
 
-        for (int i = 0; i < n_size; i++)
+        //считаем интенсивность в плоскости экрана
+        for (int i = 0; i < s_size; i++)
         {
-              f_z[i] = (int)(pow(10,9)*(pow(Re_G_table[i],2) + pow(Im_G_table[i],2)));
-               if ((int)(100*i/n_size)!=((int)(100*(i-1)/n_size))) cout << (int)(100*i/n_size) << "% intensity calculations completed"<< endl;
+            //! FIXME (Аня#1#): нужно сделать подгон интенсивностей, после того как разберемся с единицами измерения
+            f_z[i] = (int)(pow(10,13)*(pow(Re_G_table[i],2) + pow(Im_G_table[i],2)));
+
+            // прогресс бар
+            if ((int)(100*i/s_size)!=((int)(100*(i-1)/s_size))) cout << (int)(100*i/s_size) << "% intensity calculations completed"<< endl;
 
         }
 
         cout << "drawing..." << endl;
 
-       draw_mat(n_cols, n_rows, f_z);
+        draw_mat(s_cols, s_rows, f_z);
 
         cout << "still okay " << endl;
         delete [] Re_G_table;
--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@ using namespace std;
 
 
 
-void draw_mat(int w, int h, int *data){
+void draw_mat(int w, int h, int *data)
+{
     IplImage *img = cvCreateImage(cvSize(w, h), IPL_DEPTH_8U, 1);
     int i;
     for (i = 0; i < img->imageSize; i++) img->imageData[i] = data[i];
@@ -32,30 +33,24 @@ void draw_mat(int w, int h, int *data){
 int main()
 {
 
-	//тестовая матрица для начального поля
-	int h = 100;
-	int w = 100;
-	int *test = new int[h*w];
+    //тестовая матрица для начального поля
+    int h = 4;
+    int w = 4;
+    int *test = new int[h*w];
 
-	for (int i = 0; i<h*w; i++) test[i]=0;
+    for (int i = 0; i<h*w; i++) test[i]=200;
 
-	for (int i = 0; i<h*w; i++){
-        //if((i%w>40) & (i%w<47)){
-        if(i%w==45){
+    diff_system * test_sys = new diff_system(400,532, 1320000, h, w, test);
+    draw_mat(test_sys->o_cols,test_sys->o_rows,test_sys->f_0);
+    draw_mat(test_sys->s_cols,test_sys->s_rows,test_sys->f_z);
 
-            for(int j=0;j<10;j++){
-                test[i+j]=255;}
-        }
-		}
-
-   diff_system * test_sys = new diff_system(5,532, 2000000.0, h, w, test);
-   test_sys->transform();
+    test_sys->transform();
     cout << "transform sucseeded " << endl;
 
     delete [] test;
     cout << "deleted test " << endl;
-
-    cout << "not deleted sys " << endl;
+    delete test_sys;
+    cout << " deleted system " << endl;
 
     return 0;
 }
