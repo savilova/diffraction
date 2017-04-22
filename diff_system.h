564d1b2fe20ce5f28100efe5ac0e367d24e956a5
Maria <pakhnova97@mail.ru>
2017-04-22 21:00:14 +0300

    работающая версия

--- diff_system.h
+++ diff_system.h
@@ -1,10 +1,30 @@
 #ifndef DIFF_SYS
 #define DIFF_SYS
 
-#include <iostream>
-
-
+#include <cmath>
 using namespace std;
+
+//Шаблон функции create_matrix
+template <typename T>
+void create_matrix(T *** Matrix, int lines, int columns)
+{
+    (*Matrix) = new T * [lines];
+
+    for (int i = 0; i < lines; i++)
+        (*Matrix)[i] = new T [columns];
+}
+
+//Шаблон функции delete_matrix
+template <typename T>
+void delete_matrix(T *** Matrix, int columns)
+{
+    for (int i = 0; i < columns; i++)
+    {
+        delete [] ((*Matrix)[i]);
+    }
+    delete [] (*Matrix);
+
+}
 
 class diff_system
 {
@@ -18,11 +38,11 @@ public:
     double **f_z;
     double Size;
 
-    friend double ** create_matrix(int, int);
-
-
+    template <typename T> friend void create_matrix(T *** Matrix, int lines, int columns);
+    template <typename T> friend void delete_matrix(T *** Matrix, int columns);
     //конструктор
-    diff_system(int ** f){
+    diff_system(int ** f)
+    {
         wave = 532;  // длина волны в нм
         k  = 2*3.1416/wave;   // волновой вектор
         z = 2;   // расстояние от препятствия до экрана
@@ -30,122 +50,64 @@ public:
         col = 30;  // количество столбцов в матрице изображений
         f_0 = f;
         Size = 10;
-        f_z = create_matrix(row, col);
+        create_matrix(&f_z, row, col);
         cout << "Well done!" << endl;
     }
 
- /*   void create_matrix(double ** f_z, int row, int col)
-    {
-        cout << "It is create_matrix" << endl;
-        f_z = new double *[row];
-        for(int i = 0; i <row; i++)
-        {
-            f_z[i] = new double [col];
-        }
-        return;
-    }*/
-
     //деструктор
-    ~diff_system(){
-        //TODO
+    ~diff_system()
+    {
+        delete_matrix(&f_z, col);
     }
 
-    //подсчет поля в плоскости z по принципу Гюйгенса-Френеля(интеграл)
-    void transform(){
-   // std::cout << "Hello!This is transform function!" << std::endl;
-    //for (int i = 0; i<row; i++){
-      //  for(int j = 0; j<col; j++){
-        //    std::cout << f_0[i][j];
-        //}
-       // std::cout << std::endl;
-    //}
-    //std::cout << "Done!" << std::endl;
-
-        //TO DO
-        /*
-        интенсивность (вещ. число от 0 до 1) предлагаю пока умножать на 100 и округлять до интов.
-        распечатывать пока только матрицу интенсивностей, как обычно в ostream.
-        */
-        //в длинном комменте - первая версия интеграла
-
-
-        int ** Re_G_table = new int * [row];
-        for (int i = 0; i< row; i++)
-        {
-            Re_G_table[i] = new int[col];
-        }
-
-        int ** Im_G_table = new int * [row];
-        for (int i = 0; i< row; i++)
-        {
-            Im_G_table[i] = new int[col];
-        }
-
+    //Подсчет поля в плоскости z по принципу Гюйгенса-Френеля(интеграл)
+    void transform()
+    {
+        double ** Re_G_table;
+        create_matrix(&Re_G_table, row, col);
+        double ** Im_G_table;
+        create_matrix(&Im_G_table, row, col);
 
         for (int x = 0; x < row; x++)
         {
-//            cout << "X = " << x << " Row = " << row << endl;
-        for (int y = 0; y < col; y++)
-        {
-  //          cout << "Y = " << y << " Col = " << col << endl;
-            double Re_Sum = 0;
-            double Im_Sum = 0;
-            for (int r = 0; r < row; r++)
+            for (int y = 0; y < col; y++)
             {
-  //              cout << "R = " << r << " Row = " << row << endl;
-                for (int c = 0; c < col; c++)
+                double Re_Sum = 0;
+                double Im_Sum = 0;
+
+                for (int r = 0; r < row; r++)
                 {
-  //                  cout << "C = " << c << " Col = " << col << endl;
-                    double R = sqrt(pow(z,2)+pow((Size/1000)*abs((x-r)),2)+pow((Size/1000)*abs((y-c)),2));
-                    double cosinus = z/R;
-                    double Re_per_Sum = f_0[r][c] * (cosinus/R) *cos(k*pow(10,9)*R);
-                    double Im_per_Sum = f_0[r][c] * (cosinus/R) *sin(k*pow(10,9)*R);
-                    Re_Sum += Re_per_Sum*pow((Size/1000),2);
-                    Im_Sum += Im_per_Sum*pow((Size/1000),2);
+                    for (int c = 0; c < col; c++)
+                    {
+                        double R = sqrt(pow(z,2)+pow((Size/1000)*abs((x-r)),2)+pow((Size/1000)*abs((y-c)),2));
+                        double cosinus = z/R;
+                        double Re_per_Sum = f_0[r][c] * (cosinus/R) *cos(k*pow(10,9)*R);
+                        double Im_per_Sum = f_0[r][c] * (cosinus/R) *sin(k*pow(10,9)*R);
+                        Re_Sum += Re_per_Sum*pow((Size/1000),2);
+                        Im_Sum += Im_per_Sum*pow((Size/1000),2);
+                    }
                 }
+
+                Re_G_table[x][y] = Re_Sum*pow(10,9)/wave;
+                Im_G_table[x][y] = Im_Sum*pow(10,9)/wave;
             }
-            Re_G_table[x][y] = Re_Sum*pow(10,9)/wave;
-            Im_G_table[x][y] = Im_Sum*pow(10,9)/wave;
         }
-        }
-        std::cout<<"For end!"<<std::endl;
+
+        cout<<"For end!"<<endl;
         for (int x = 0; x < row; x++)
         {
-            cout << "X = " << x << " Row = " << row << endl;
-        for (int y = 0; y < col; y++)
-        {
-            cout << "pow(Re_G_table[x][y],2) = " << pow(Re_G_table[x][y],2) << endl;
-            cout << "pow(Im_G_table[x][y],2) = " << pow(Im_G_table[x][y],2) << endl;
-            cout << "f_z[x][y] = " << f_z[x][y] << endl;
-            f_z[x][y] = pow(Re_G_table[x][y],2) + pow(Im_G_table[x][y],2);
-            std::cout<<"Hi!"<<std::endl;
-            std::cout<<f_z[x][y]<<' ';
-        }
-            std::cout << std::endl;
+
+            for (int y = 0; y < col; y++)
+            {
+                f_z[x][y] = pow(Re_G_table[x][y],2) + pow(Im_G_table[x][y],2);
+                cout<<f_z[x][y]<<' ';
+            }
+            cout << endl;
         }
 
-        for (int i=0;i<row;i++)
-        {
-            delete [] Re_G_table[i];
-        }
-        delete [] Re_G_table;
-
-
-        for (int i=0;i<row;i++)
-        {
-            delete [] Im_G_table[i];
-        }
-        delete [] Im_G_table;
-
-}
+        delete_matrix(&Re_G_table, col);
+        delete_matrix(&Im_G_table, col);
+    }
 };
 
-double ** create_matrix(int row, int col)
-{
-        double **f = new double *[row];
-        for(int i = 0; i <row; i++)
-            f[i] = new double [col];
-
-        return f;
-}
 #endif // DIFF_SYS
--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <math.h>
 #include <cmath>
 #include "diff_system.h"
 using namespace std;
@@ -9,19 +8,19 @@ int main()
 {
 
 	int length = 30;
-	int **test = new int *[length];
-	for(int i = 0; i <length; i++)
-        test[i] = new int[length];
+	int **test;
+	create_matrix(&test, length, length);
 
-	for (int i = 0; i<length; i++){
-		for(int j = 0; j<length; j++){
-			if (j<(length/2)||j>(length/2)) {
+	for (int i = 0; i<length; i++)
+	{
+		for(int j = 0; j<length; j++)
+		{
+			if (j<(length/2)||j>(length/2))
 				test[i][j]= 0;
-			}
-			else {
+			else
 			test[i][j]= 1;
-		}
-		//ее вывод
+
+			//Вывод матрицы
 			cout << test[i][j];
 		}
 		cout << endl;
@@ -30,11 +29,7 @@ int main()
 	diff_system test_sys(test);
 	test_sys.transform();
 
-	for (int i=0;i<length;i++)
-        {
-            delete [] test[i];
-        }
-    delete [] test;
+	delete_matrix(&test, length);
 
        return 0;
 }
