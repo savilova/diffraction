4df3a901902689bdfb669a31c7930e3a42a4c8d0
PakhnovaMaria <pakhnova97@mail.ru>
2017-05-15 18:33:43 +0300

    поправки в diff_system

--- diff_system.h
+++ diff_system.h
@@ -2,27 +2,27 @@
 #define DIFF_SYS
 #include "obstacle.h"
 
-void draw_mat(int Wave, int h, int *data);
+void draw_mat(int w, int h, int *data);
 
 class diff_system
 {
 public:
     //Свойства света
     double magnitude; //амплитуда падающей плоской волны
-     double wave; // Длина волны в нанометрах
-     double k; // Волновое число, 1/нм
+    double wave; // Длина волны в нанометрах
+    double k; // Волновое число, 1/нм
     //Свойства экрана
     double pixel; // Физический размер пикселя результирующей картины в микронах
-     int width, size; // Числа рассматриваемых отрезков d_x, d_y и клеток на экране
+    int width, size; // Числа рассматриваемых отрезков d_x, d_y и клеток на экране
     //Прочее
     double z; // Расстояние от (0,0) на препятствии до (0,0) на экране, мкм
-    obstacle * obs; // препятствие, которое мы ставим в систему
-    int * intensity; // Матрица интенсивности (конечная картинка)
+    obstacle * o; // препятствие, которое мы ставим в систему
+    int * Intensity; // Матрица интенсивности (конечная картинка)
 
     // Конструктор по умолчанию
     diff_system();
     // Конструктор
-    diff_system(int magnitude, int s_pixel_size, int wave, double z, obstacle * obs);
+    diff_system(int magnitude, int pixel, int wave, double z, obstacle * o);
 
     // Деструктор
     ~diff_system(void);
--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <fstream>
-#include <math.h>
-#include <cmath>
+
 #include "diff_system.h"
-#include "cv.h"
-#include "highgui.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 using namespace std;
 
 void draw_mat(int width, int length, int *data)
 {
     IplImage *img = cvCreateImage(cvSize(width, length), IPL_DEPTH_8U, 1);
-    int i;
-    for (i = 0; i < img->imageSize; i++)
+
+    for (int i = 0; i < img->imageSize; i++)
         img->imageData[i] = data[i];
 
     // Создаем окно с заголовком image
@@ -21,7 +20,7 @@ void draw_mat(int width, int length, int *data)
     // Ждем нажатия юбой клавиши
     cvWaitKey (0);
 
-    // Освобождаем память перд выходм из программы
+    // Освобождаем память перд выходом из программы
     cvReleaseImage (&img);
     cvDestroyWindow("image");
 
@@ -29,34 +28,10 @@ void draw_mat(int width, int length, int *data)
 
 int main()
 {
-    // Матрица препядствия, где светлые точки это отверстие
-    // Всё остальное это тёмные отверстия
-    // В данной версии отверстие прямоугольное
 
-    // Размер пикселя препядствия в микронах
-    int o_pixel_size = 25;
 
-    // Размер пикселя результирующей картины в микронах
-    int s_pixel_size = 100;
-
-    int wave = 532;  // Длина волны в нанометрах
-    int R = 1000000; // Расстояние до экрана в мкм
-    int length = 6; // Длина отверстия
-    int width = 6;  // Ширина отверстия
-    int *obstacle = new int[length*width]; // Матрица препядствия
-
-    for (int i = 0; i < length*width; i++)
-        obstacle[i] = 200; // Яркость точек отверстия 200
-
-    diff_system * Screen = new diff_system(o_pixel_size, s_pixel_size, wave, R, length, width, obstacle);
-    draw_mat(Screen->o_cols,Screen->o_rows,Screen->Obstacle);
-    draw_mat(Screen->s_cols,Screen->s_rows,Screen->Intensity);
-
-    Screen->transform();
-    cout << "transform sucseeded " << endl;
-
-    delete [] obstacle;
-    cout << "deleted obstacle " << endl;
+diff_system * test = new diff_system();
+test->transform();
 
     return 0;
 }
